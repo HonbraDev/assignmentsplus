c74b6881a7305c98dd090d7982b6563afe2a19c6
Honbra <honbra@honbra.com>
2022-04-13 23:07:17 +0200

    Landing / login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import { Button } from "@mui/material";
+import Dashboard from "@components/dashboard";
+import Login from "@components/login";
+
 import {
   AuthenticatedTemplate,
   UnauthenticatedTemplate,
-  useMsal,
 } from "@azure/msal-react";
-import { loginRequest } from "@config/authConfig";
-import Dashboard from "./components/dashboard";
-import { useEffect } from "react";
 
 function App() {
-  const { instance } = useMsal();
   return (
     <>
       <AuthenticatedTemplate>
         <Dashboard />
       </AuthenticatedTemplate>
       <UnauthenticatedTemplate>
-        <Button onClick={() => instance.loginPopup(loginRequest)}>
-          sign in sussy boy
-        </Button>
+        <Login />
       </UnauthenticatedTemplate>
     </>
   );
---
+++ src/components/login.tsx
@@ -0,0 +1,71 @@
+import { Box, Button, Card, Link, Typography } from "@mui/material";
+import { loginRequest } from "@config/authConfig";
+import { useMsal } from "@azure/msal-react";
+
+function Login() {
+  const { instance } = useMsal();
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        inset: "0",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          width: "min-content",
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+        }}
+      >
+        <Typography variant="h4" textAlign="center">
+          Assignments+
+        </Typography>
+        <Card
+          sx={{
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            width: 400,
+          }}
+        >
+          <Box>
+            <Typography variant="body1" gutterBottom>
+              Please sign in to view your assignments.
+            </Typography>
+            <Typography variant="body1">
+              Only internal accounts from the mensagym tenant are currently
+              supported due to API restrictions.
+            </Typography>
+          </Box>{" "}
+          <Button
+            variant="contained"
+            onClick={() => instance.loginPopup(loginRequest)}
+            sx={{
+              marginLeft: "auto",
+              display: "block",
+            }}
+          >
+            Sign in with Microsoft
+          </Button>
+        </Card>
+        <Box>
+          <Typography variant="body1" textAlign="center" color="text.secondary">
+            Assignments+ is free and open-source software powered by Microsoft
+            Teams.{" "}
+            <Link href="https://github.com/honbradev/assignmentsplus">
+              View&nbsp;the&nbsp;source&nbsp;code
+            </Link>
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default Login;
